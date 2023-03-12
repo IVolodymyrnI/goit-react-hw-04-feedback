3670a9c4431a2a1fb22551404849ad6a09f7a63a
IVolodymyrnI <volodia.iarcguk.forwork@gmail.com>
2023-03-12 15:21:42 +0200

    react-02-feedback

--- src/components/Feedback/Notification/Notification .jsx
+++ src/components/Feedback/Notification/Notification .jsx
@@ -1,5 +1,11 @@
 import { Message } from './Notification';
 
+import PropTypes from 'prop-types';
+
 export function Notification({ message }) {
   return <Message>{message}</Message>;
 }
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
