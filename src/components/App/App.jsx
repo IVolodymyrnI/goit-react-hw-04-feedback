7d5dd50b7d21aa96985a96d27fc8429f21079c5b
IVolodymyrnI <volodia.iarcguk.forwork@gmail.com>
2023-03-22 16:00:07 +0200

    react-04-feedback

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useState } from 'react';
+import { useState } from 'react';
 import { FeedbackForm } from 'components/Feedback/FeedbackForm/FeedbackForm';
 import { FeedbackSection } from 'components/Feedback/FeedbackSection/FeedbackSection';
 import { Statistics } from 'components/Feedback/Statistics/Statistics';
@@ -60,55 +60,3 @@ export const App = () => {
     </FeedbackSection>
   );
 };
-
-export class OldApp extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  onSubmitOption = value => {
-    this.setState(prevState => ({ [value]: prevState[value] + 1 }));
-  };
-
-  countTotalFeedback(state) {
-    const arrayFeedbacks = Object.values(state);
-    return arrayFeedbacks.reduce((acc, number) => acc + number, 0);
-  }
-
-  countPositiveFeedbackPercentage({ total, good }) {
-    const positivePercentage = Math.round((good / total) * 100);
-    return `${positivePercentage}%`;
-  }
-
-  render() {
-    const { good, bad, neutral } = this.state;
-    const isFeedback = Boolean(good || bad || neutral);
-    const total = this.countTotalFeedback(this.state);
-    const positivePercentage = this.countPositiveFeedbackPercentage({
-      good,
-      total,
-    });
-
-    return (
-      <FeedbackSection title="Will you come back?">
-        <FeedbackForm
-          onSubmit={value => this.onSubmitOption(value)}
-          data={emojis}
-        />
-        {isFeedback ? (
-          <Statistics
-            good={good}
-            bad={bad}
-            neutral={neutral}
-            total={total}
-            positivePercentage={positivePercentage}
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </FeedbackSection>
-    );
-  }
-}
